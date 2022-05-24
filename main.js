e5a78c26bf31824d9b5849549ea8c528fe9a1459
Vincasmen <vincasmen13@gmail.com>
2022-05-24 22:58:32 +0200

    Without a change just trying some stuff with alert message when empty input.

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+//Trying to figure out why are values coming back as strings instead of numbers so I can apply If Else for Aler message
+
+// const lengthCilinder = document.getElementById("length-of-the-cilinder");
+// const submitBtn = document.getElementById("submit-button");
+
+// submitBtn.addEventListener("click", function () {
+//   input();
+//   console.log(typeof lengthCilinder);
+// });
+
+// const input = function () {
+//   lengthCilinder.value;
+// };
+
 window.onload = function () {
   document.getElementById("submit-button").onclick = function () {
     const pi = "3.141592654";
@@ -5,6 +19,13 @@ window.onload = function () {
       cilinderLength = document.getElementById("length-of-the-cilinder").value,
       output = document.getElementById("output-box");
 
+    //Failed attempt for If else statement for alert message
+
+    // if (outerDiameter.value <= 0) {
+    //   alert("!!!");
+    // console.log(typeof outerDiameter, cilinderLength, pi);
+    // } else {
     output.innerHTML = (pi * outerDiameter * cilinderLength).toFixed(2);
   };
 };
+// };
